66d7e5baab47261588e456f992eaa00ba095ac72
Nycgia <pmunozb@protonmail.com>
2022-06-25 01:37:23 +0000

    Deploying to gh-pages from @ Nycgia/mobiliaria-nr@48f42fe505ee8ff1c08b482fad6e1dba989b176e 🚀

--- _next/static/2vel9WTnKSy7TgLHr0kOa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/905e927be80ff408.css","static/chunks/pages/index-e4d52ce093be461a.js"],"/_error":["static/chunks/pages/_error-815e492bede44f3e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
